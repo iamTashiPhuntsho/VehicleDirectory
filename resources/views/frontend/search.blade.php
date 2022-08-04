91c65cd59be102c09f0e4544daebf499ee9f05df
Tashi Phuntsho <taphin@bnb.bt>
2022-08-04 12:49:45 +0600

    Employee Directory V2 Initiated

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -1,4 +1,4 @@
 <x-frontend-layout>
     <h1 class="title"><i class="fa-solid fa-magnifying-glass"></i> Search Will be here</h1>
-    <p>Searching feature will be built here</p>
+    <p>Searching features will be built here</p>
 </x-frontend-layout>
