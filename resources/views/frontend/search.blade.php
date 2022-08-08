c3055fb97bd89e115a39d7d9412725867dcc1d6c
CheningYangden <ugyentendi7@gmail@.com>
2022-08-08 12:52:31 +0600

    side bar

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -1,4 +1,6 @@
 <x-frontend-layout>
+    <x-sidebar />
+    <x-test-component />
     <h1 class="title"><i class="fa-solid fa-magnifying-glass"></i> Search Will be here</h1>
     <p>Searching feature will be built here</p>
 </x-frontend-layout>
