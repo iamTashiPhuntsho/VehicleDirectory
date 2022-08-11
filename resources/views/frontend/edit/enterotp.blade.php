3fb03f8023222d76cd2c20875fbe846ac595a199
Shuga619 <wingchuester1619@gmail.com>
2022-08-11 12:22:32 +0600

    mail signature and otp fixed

--- resources/views/frontend/edit/enterotp.blade.php
+++ resources/views/frontend/edit/enterotp.blade.php
@@ -1,28 +1,5 @@
 <x-frontend-layout>
     <x-sidebar />
-  <nav class="navbar navbar-expand-lg navbar-dark {{$no == 1 ? 'bg-bnb-orange' : 'bg-bnb-blue'}} fixed-top" id="sideNav">
-        <a class="navbar-brand js-scroll-trigger" href="#page-top">
-          <span class="d-block d-lg-none">Search Employee</span>
-          <span class="d-none d-lg-block">
-              <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="{{asset ('images/bnb.png')}}" alt="">
-          </span>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-              <li class="nav-item mb-5">
-                <h3 class="no-case {{$no == 1 ? 'text-bnb-blue' : 'text-bnb-orange'}}">
-                  Employee Directory 
-                </h3>
-              </li>
-              <li class="nav-item">
-                <small class="text-white"><b>Built By : <br> BNBL IT Department <br>2019</b></small>
-              </li>
-          </ul>
-        </div>
-    </nav>
 
     <div class="container-fluid p-0">
         <section class="search-section p-3 p-lg-5 d-block d-flex d-column {{$no == 1 ? 'bg-bnb-blue' : 'bg-bnb-orange'}}">
