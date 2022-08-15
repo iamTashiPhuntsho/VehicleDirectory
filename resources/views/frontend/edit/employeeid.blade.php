7a4e2c79e32dfd155101694c9f6d8486452927ca
Tashi Phuntsho <taphin@bnb.bt>
2022-08-15 12:21:07 +0600

    OTP fix

--- resources/views/frontend/edit/employeeid.blade.php
+++ resources/views/frontend/edit/employeeid.blade.php
@@ -1,16 +1,16 @@
 <x-frontend-layout>
     <x-sidebar />
-            <div class="p-5">
-          		<h2 class="no-case mb-5 ">Edit Your Profile On Employee Directory</h2>
-              <div class="mb-5">
-                <div class="col-sm-12">
-                    <p class="u-large-text u-text u-text-variant u-text-2"> 
-                You can edit the Basic fields like Name, Email ID. You can also edit the work information like Employee ID, Employee Status, etc., Other things you can edit includes, 
-                Contact Information, Notice Period Information, EEO Roles. 
-				        </p>
-                </div>
-				        
-			        </div>
+    <div class="p-5">
+      <h2 class="no-case mb-5 ">Edit Your Profile On Employee Directory</h2>
+      <div class="mb-5">
+        <div class="col-sm-12">
+            <p class="u-large-text u-text u-text-variant u-text-2"> 
+        You can edit the Basic fields like Name, Email ID. You can also edit the work information like Employee ID, Employee Status, etc., Other things you can edit includes, 
+        Contact Information, Notice Period Information, EEO Roles. 
+        </p>
+        </div>
+        
+      </div>
       <div class="col-lg-12">
         <div class="card text-black" style="border-radius: 15px;">
           <div class="card-body">
@@ -18,7 +18,8 @@
               <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
 
                 <p class="text-center h4 fw-bold mb-5 mx-1 mx-md-4 mt-4">Edit Here</p>
-                <form class="mx-1 mx-md-4">
+                <form class="mx-1 mx-md-4" method="POST" action="{{ route('get_employee_and_send_otp_path') }}">
+                  @csrf
                   <div class="d-flex flex-row align-items-center mb-4">
                     <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                     <div class="form-outline flex-fill mb-0">
