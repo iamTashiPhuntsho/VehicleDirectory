42004397e467d8f603db1bd97ead0a0edafccfa4
CheningYangden <ugyentendi7@gmail@.com>
2022-08-18 13:28:42 +0600

    font-weight fix and generate-verify

--- resources/views/signature/verify.blade.php
+++ resources/views/signature/verify.blade.php
@@ -4,19 +4,23 @@
     <div class="container">
       <section class="p-5">
         <div class="d-flex flex-column center">
-          		<h3 class="title2 mb-4">Mail Signature Generator</h3>
+          		<h3 class="title2 mb-3">Mail Signature Generator</h3>
           		<div class="">
           			@if($flag)
-                  <p class="text-center small mb-4">
-                    Check the Signature Preview below and modify the information if required. Keep the field(s) as it is, if no modification in required. All the empty fields will not be included in the Signature Code.</p>
+                <div class="mb-3">
+                        <p class="u-large-text u-text u-text-variant u-text-2"> 
+                        Check the Signature Preview below and modify the information if required. Keep the field(s) as it is, if no modification in required. All the empty fields will not be included in the Signature Code.</p>
+                        </p>
+                     </div>
+                 
                   <div class="preview-container">
                     <div class="clearfix">
                       <form action="{{ route('get_signature_code_path') }}" method="GET">
-                        <div class="border p-5 clearfix">
-                          <div class=""style="font-size: 12pt; font-family: sans-serif; line-height: 1.2; margin-bottom: 12px;">
+                        <div class="border p-5 clearfix" style="border-radius:15px;">
+                          <div class=""style="font-size: 12pt; font-family: sans-serif; line-height: 1.2; margin-bottom: 12px; font-weight: normal; padding-top:20px;">
                               Thank you, &amp; Best regards.
                             </div>
-                            <div style= "font-family: 'Century Gothic', sans-serif; color: black; line-height: 1.2;">
+                            <div style= "font-family: 'Century Gothic', sans-serif; color: black; line-height: 1.2; font-weight: normal;">
                               <span style="font-size: 11pt;">
                                 <strong>
                                   {{$emp->name}} </strong> &nbsp; (<select name="salutation">
@@ -26,7 +30,7 @@
                                     <option>Mrs.</option>
                                   </select>)
                               </span><br />
-                              <span style="font-size: 10pt;">
+                              <span style="font-size: 10pt; ">
                                 {{$emp->title}}<br />
                                 {{$emp->department->name}} Department
                               </span>
@@ -35,7 +39,7 @@
   
                             <div style="max-width: 500px; padding: 0px; margin-top: 12px; margin-bottom: 10px; display: inline-flex;">
                               <div style="float: left;">
-                                <span style="font-size: 11pt; font-family: 'Raleway ExtraBold', sans-serif; color: black; line-height: 1.6;">
+                                <span style="font-size: 11pt; font-family: 'Raleway ExtraBold', sans-serif; color: black; line-height: 1.6; font-weight: normal;">
                                   <strong>BHUTAN NATIONAL BANK LTD.</strong>
                                 </span><br>
                                 <span style="font-size: 10pt; font-family: 'Century Gothic', sans-serif; color: black; line-height: 1;">
@@ -52,7 +56,7 @@
                             </div>
   
                             <div class="mb-5">
-                              <p style="float: right; font-family: 'Century Gothic', sans-serif; font-size: 8pt; color: #808080; line-height: 1.6;">
+                              <p style="float: right; font-family: 'Century Gothic', sans-serif; font-size: 8pt; color: #808080; line-height: 1.6; font-weight: normal;" >
                                 The contents of this e-mail and any attachment(s) are confidential and intended for the named recipient(s) only. Any form of reproduction, dissemination, copying, disclosure, modification, distribution and/or publication of this message without the prior written consent of the author of this e-mail is strictly prohibited. If you have received this email in error please delete it and notify the sender immediately, so that we can ensure such a mistake does not occur in the future. Before opening any mail and attachments please check them for viruses and defects. Any views or opinions presented in this email are solely those of the author and may not necessarily reflect the opinions of BNBL.
                               </p>
                             </div>
